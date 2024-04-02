e6fbf06fa33f9ccbe5dc1d57800ea4d73d07f3a0
40388825 <cmcgloin02@qub.ac.uk>
2024-04-02 21:04:57 +0100

    Board Game Test

---
+++ test/beeopoly/BoardGameTest.java
@@ -0,0 +1,45 @@
+package beeopoly;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BoardGameTest {
+	
+	Player player;
+	List<Player> activePlayers;
+    List<Player> playerRank;
+    List<Garden> gardens;
+
+	@BeforeEach
+	void setUp() throws Exception {
+
+		player = new Player("Test Name");
+		activePlayers = new ArrayList<>();
+        playerRank = new ArrayList<>();
+        gardens = new ArrayList<>();
+        activePlayers.add(player);
+        
+	}
+
+
+	@Test
+	void testRemovePlayer() {
+		
+		//Check player is in the ArrayList
+		assertTrue(activePlayers.contains(player));
+		//remove the player, check if he has been removed
+		activePlayers.remove(player);
+		assertFalse(activePlayers.contains(player));
+		// add the player to the rank
+		playerRank.add(player);
+		assertTrue(playerRank.contains(player));
+		
+	}
+
+
+}
